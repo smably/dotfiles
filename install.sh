22445a90de97d7a7f22af9708fab92e9f7d6b887
Sylvan Mably <sylvan@gmail.com>
2018-06-30 15:02:32 -0400

    Prompt before homebrew installation

--- install.sh
+++ install.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
 # Install all the things!
-echo "Installing Homebrew formulas..."
-brew install $(< brew-formulas.list)
-echo "Done Homebrew formulas"
+echo "Installing the following homebrew formulas:"
+echo $(< brew-formulas.list)
 echo
+read -p "Would you like to continue (Y/n)? " answer
+case ${answer:0:1} in
+  n|N )
+    echo
+  ;;
+  * )
+    echo "Installing Homebrew formulas..."
+    brew install $(< brew-formulas.list)
+    echo "Done Homebrew formulas"
+    echo
+  ;;
+esac
 
 # Install all the other things!
-echo "Installing Homebrew casks..."
-brew cask install $(< brew-casks.list)
-echo "Done Homebrew casks"
+echo "Installing the following homebrew casks:"
+echo $(< brew-casks.list)
 echo
+read -p "Would you like to continue (Y/n)? " answer
+case ${answer:0:1} in
+  n|N )
+    echo
+  ;;
+  * )
+    echo "Installing Homebrew casks..."
+    brew cask install $(< brew-casks.list)
+    echo "Done Homebrew casks"
+    echo
+  ;;
+esac
 
 # Install powerline-go (not available in Homebrew aaargh!)
 POWERLINE_GO_PATH="$HOME/go/bin/powerline-go" # TODO use $GOPATH if it's set?
