6953be4049d42992b3862501ed7779a28f209a6b
Sylvan Mably <sylvan@gmail.com>
2021-03-22 14:27:08 -0400

    Remove outdated install logic

--- install.sh
+++ install.sh
@@ -32,20 +32,6 @@ n | N)
   ;;
 esac
 
-# Install VS Code Settings Sync
-read -p "Would you like to install VS Code Settings Sync (Y/n)? " answer
-case ${answer:0:1} in
-n | N)
-  ;;
-*)
-  VS_CODE_CONFIG_DIR="$HOME/Library/Application Support/Code/User"
-  alias code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-  code --install-extension Shan.code-settings-sync
-  echo "Done installing VS Code Settings Sync (open VS Code and run <shift>-<opt>-D to sync settings)"
-  echo
-  ;;
-esac
-
 # Install powerline-go (not available in Homebrew aaargh!)
 read -p "Would you like to install powerline-go (Y/n)? " answer
 case ${answer:0:1} in
@@ -59,22 +45,8 @@ n | N)
   ;;
 esac
 
-# Install iTerm2 preferences
-read -p "Would you like to install iTerm2 preferences (Y/n)? " answer
-case ${answer:0:1} in
-n | N)
-  ;;
-*)
-  echo "Setting default iTerm2 preferences folder to $ROOT_DIR"
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$ROOT_DIR"
-  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-  echo "Done iTerm2 preferences"
-  echo
-  ;;
-esac
-
 # Restore backed up settings
-read -p "Would restore dotfiles from Dropbox using Mackup (Y/n)? " answer
+read -p "Would you like to restore configs from private dotfiles repo using Mackup (Y/n)? " answer
 case ${answer:0:1} in
 n | N)
   ;;
